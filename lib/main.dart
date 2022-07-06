3e69c9b5f325964c719ab60135acb3a5cad3d99f
Manas <manasjagtap20@gmail.com>
2022-07-06 18:23:49 +0530

    day2nday3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:learningdart/home_page.dart';
+import 'package:learningdart/pages/home_page.dart';
+import 'package:learningdart/pages/login_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Homepage());
+    return MaterialApp(
+      // home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home":(context) => Homepage(),
+        "/login":(context) => LoginPage(), 
+      },
+    );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      child: Center(
+        child: Text("Login Page",
+        style: TextStyle(
+          fontSize: 40,
+          color: Colors.purple,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic
+        ),
+        textAlign: TextAlign.right
+        
+        ),
+      ),
+    );
+  }
+}
