a20d18ac72877d57640984ce50f388c895cb7e4a
Manas <manasjagtap20@gmail.com>
2022-07-06 20:08:23 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learningdart/pages/home_page.dart';
 import 'package:learningdart/pages/login_page.dart';
+import "package:google_fonts/google_fonts.dart";
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -12,15 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: Homepage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          fontFamily: GoogleFonts.robotoMono().fontFamily),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        "/home":(context) => Homepage(),
-        "/login":(context) => LoginPage(), 
+        "/home": (context) => Homepage(),
+        "/login": (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:learningdart/pages/home_page.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
-        style: TextStyle(
-          fontSize: 40,
-          color: Colors.purple,
-          fontWeight: FontWeight.bold,
-          fontStyle: FontStyle.italic
-        ),
-        textAlign: TextAlign.right
-        
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue.shade800,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Hemlo");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
